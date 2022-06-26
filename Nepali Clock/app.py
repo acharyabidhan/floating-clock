41b9290a394afefe2a50d337c6d0f0f11e87a64b
acharyabidhan <acharyaumesh2005@gmail.com>
2022-06-26 18:16:52 +0545

    new features

--- Nepali Clock/app.py
+++ Nepali Clock/app.py
@@ -1,9 +1,9 @@
-import random, time, nepali_datetime
 from tkinter import*
-from tkinter import Canvas
-import pythoncom
-from win32com.client import Dispatch
 from os import path
+from tkinter import Canvas
+from win32com.client import Dispatch
+import time, nepali_datetime, pythoncom, darkdetect
+
 app = Tk()
 app.overrideredirect(True)
 app.resizable(0,0)
@@ -14,19 +14,35 @@ screen_height = app.winfo_screenheight()
 position_top = 10
 position_right = int(screen_width - (width+10))
 app.geometry(f'{width}x{height}+{position_right}+{position_top}')
-bgColor = "blue"
-app.config(background="black")
+
+bgColor = None
+fgColor = None
+
+if darkdetect.isDark():
+    bgColor = "black"
+    fgColor = "white"
+else:
+    bgColor = "white"
+    fgColor = "black"
+
+transparentColor = "pink"
+app.config(background=transparentColor)
+
+######################################################
+#Variables for creating shortcut
 username = (path.split(path.expanduser('~'))[-1])
-# Just extract and put the main folder (which contains .exe and other dependencies) in C drive
 appName = "nep-clock"
 target = f"{appName}\\{appName}.exe"
 wDir = f"C:\\{appName}"
+######################
 pythoncom.CoInitialize()    
 shell = Dispatch("WScript.Shell")
 shortcut = shell.CreateShortCut(f"C:\\Users\\{username}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\{appName}.lnk")
 shortcut.Targetpath = target
 shortcut.WorkingDirectory = wDir
 shortcut.save()
+######################################################
+
 def round_rectangle(x1, y1, x2, y2, radius, color, **kwargs):
     points = [
             x1+radius, y1,
@@ -51,13 +67,20 @@ def round_rectangle(x1, y1, x2, y2, radius, color, **kwargs):
             x1, y1
             ]
     return canvas.create_polygon(points, **kwargs, smooth=True, fill=color)
-canvas = Canvas(app, bg="black", highlightthickness=0)
+
+canvas = Canvas(app, bg=transparentColor, highlightthickness=0)
 canvas.pack(fill=BOTH, expand=1)
+
 round_rectangle(0, 0, width, height, radius=50, color=bgColor)
+
 offsetx = 0
 offsety = 0
 currentX = position_right
 currentY = position_top
+
+lightColor = "white"
+darkColor = "black"
+
 def drag(event):
     global currentX, currentY
     x = app.winfo_pointerx() - app.offsetx
@@ -65,16 +88,37 @@ def drag(event):
     app.geometry('+{x}+{y}'.format(x=x,y=y))
     currentX = x
     currentY = y
+
 def click(event):
     app.offsetx = event.x
     app.offsety = event.y
-colors = ["red", "green", "blue"]
-def changeColor():
-    color = random.choice(colors)
-    round_rectangle(0, 0, width, height, radius=50, color=color)
-    timeLabel.config(background=color)
-    dateLabel.config(background=color)
-    dayLabel.config(background=color)
+
+def savePstate(bg):
+    pStateFile = open("pstate", "w")
+    pStateFile.write(bg)
+    pStateFile.close()
+
+def lightTheme():
+    round_rectangle(0, 0, width, height, radius=50, color=lightColor)
+    timeLabel.config(background=lightColor, foreground=darkColor)
+    dateLabel.config(background=lightColor, foreground=darkColor)
+    dayLabel.config(background=lightColor, foreground=darkColor)
+    savePstate("light")
+
+def darkTheme():
+    round_rectangle(0, 0, width, height, radius=50, color="black")
+    timeLabel.config(background=darkColor, foreground=lightColor)
+    dateLabel.config(background=darkColor, foreground=lightColor)
+    dayLabel.config(background=darkColor, foreground=lightColor)
+    savePstate("dark")
+
+def makeTransparent():
+    round_rectangle(0, 0, width, height, radius=50, color=transparentColor)
+    timeLabel.config(background=transparentColor, foreground=lightColor)
+    dateLabel.config(background=transparentColor, foreground=lightColor)
+    dayLabel.config(background=transparentColor, foreground=lightColor)
+    savePstate("clear")
+
 def keySc(e):
     global currentX, currentY
     key = e.keysym
@@ -83,20 +127,27 @@ def keySc(e):
         app.geometry(f'{width}x{height}+{position_right}+{position_top}')
         currentX = position_right
         currentY = position_top
+
     elif key == "Up" and currentY > 0:
         currentY = currentY - increment
         app.geometry(f"+{currentX}+{currentY}")
+
     elif key == "Down"  and currentY < (screen_height - height):
         currentY = currentY + increment
         app.geometry(f"+{currentX}+{currentY}")
+
     elif key == "Left" and currentX > 0:
         currentX = currentX - increment
         app.geometry(f"+{currentX}+{currentY}")
+
     elif key == "Right" and currentX < (screen_width - width):
         currentX = currentX + increment
         app.geometry(f"+{currentX}+{currentY}")
-    if key == "c" or key == "C":
-        changeColor()
+
+    elif key == "d" or key == "D":darkTheme()
+    elif key == "l" or key == "L":lightTheme()
+    elif key == "t" or key == "T":makeTransparent()
+
 def converToNepali(n):
     engNum = str(n)
     nepNumList = []
@@ -112,6 +163,7 @@ def converToNepali(n):
         elif i == "9":nepNumList.append("९")
         else:nepNumList.append("०")
     return "".join(nepNumList)
+
 def updateTime():
     cHr = converToNepali(time.strftime("%I"))
     cMin = converToNepali(time.strftime("%M"))
@@ -126,18 +178,33 @@ def updateTime():
     dateLabel.config(text=cDate)
     dayLabel.config(text=cDay)
     app.after(1000, updateTime)
-timeLabel = Label(app, font=("arial",30), background=bgColor, foreground="white")
+
+timeLabel = Label(app, font=("arial",30), background=bgColor, foreground=fgColor)
 timeLabel.place(relx=0.50, rely=0.02, anchor=N)
-dateLabel = Label(app, font=("arial",20), background=bgColor, foreground="white")
+
+dateLabel = Label(app, font=("arial",20), background=bgColor, foreground=fgColor)
 dateLabel.place(relx=0.50, rely=0.50, anchor=CENTER)
-dayLabel = Label(app, font=("arial",20), background=bgColor, foreground="white")
+
+dayLabel = Label(app, font=("arial",20), background=bgColor, foreground=fgColor)
 dayLabel.place(relx=0.50, rely=0.90, anchor=S)
+
+if path.isfile("pstate"):
+    pStateDetail = open("pstate", "r")
+    whatToDo = pStateDetail.read()
+    pStateDetail.close()
+    if whatToDo == "light":lightTheme()
+    elif whatToDo == "dark":darkTheme()
+    elif whatToDo == "clear":makeTransparent()
+
 updateTime()
+
 def doNothing():pass
-app.wm_attributes("-transparentcolor", "black")
+
+app.wm_attributes("-transparentcolor", "pink")
 app.bind("<Button-1>", click)
 app.bind("<B1-Motion>", drag)
 app.bind("<KeyPress>", keySc)
 app.protocol("WM_DELETE_WINDOW", doNothing)
+
 app.mainloop()
 #Made with ❤ by Bidhan Acharya
